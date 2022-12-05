9db4e85137be81a607d5efccccd76eb780ce331f
Camilo Restrepo <camilorg2017@gmail.com>
2022-12-05 10:54:47 -0500

    configuration angular fire library
    configuration angular fire library

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.services';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -11,14 +11,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
   constructor(
     private trainingService: TrainingService,
     private db: AngularFirestore
   ) {}
 
   ngOnInit() {
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            const data: any = doc.payload.doc.data();
+            return {
+              id: doc.payload.doc.id,
+              ...data,
+            };
+          });
+        })
+      );
   }
 
   onStartTraining(form: NgForm) {
